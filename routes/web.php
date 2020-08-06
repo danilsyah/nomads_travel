d547c2ad06fbc1d09a2cb0c525b7ad99cc551c83
danil syah <danilsyaharihardjo@gmail.com>
2020-08-06 16:15:58 +0700

    auth middleware login

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,11 @@ Route::get('/checkout/success', 'CheckoutController@success')
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth','admin'])
     ->group(function () {
         Route::get('/', 'DashboardController@index')
             ->name('dashboard');
     });
 
+
+Auth::routes(['verify' => true]);
